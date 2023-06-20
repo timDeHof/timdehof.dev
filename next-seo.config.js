974f3e5ad0a045fa87cbcbd0f92592a7b1005327
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-20 09:26:56 -0400

    feat: add SEO configuration to the website
    Add SEO configuration to the website using the `next-seo` package. The
    configuration includes the website title, description, canonical URL,
    open graph metadata, and images. The `DefaultSeo` component is added to
    the `_app.tsx` file to apply the configuration to all pages.
    
    Also, add the required images and manifest file to the public directory.
    
    The `clsx` package is also added to the project dependencies.

---
+++ next-seo.config.js
@@ -0,0 +1,25 @@
+export default {
+  title: "timdev | Tim DeHof",
+  description: "Real full-stack ninja lives here.",
+  canonical: "https://timdehof.dev",
+  openGraph: {
+    url: "https://timdehof.dev",
+    title: "timdev | Tim DeHof",
+    description: "Real full-stack ninja lives here.",
+    images: [
+      {
+        url: "/public/android-chrome-512x512.png",
+        width: 512,
+        height: 512,
+        alt: "Og Image Alt",
+      },
+      {
+        url: "/public/android-chrome-192x192.png",
+        width: 192,
+        height: 192,
+        alt: "Og Image Alt 2",
+      },
+    ],
+    site_name: "timdev | Tim DeHof",
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,14 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
+import { DefaultSeo } from "next-seo";
 
+import SEO from "../next-seo.config.js";
 const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <DefaultSeo {...SEO} />
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools />
         <Component {...pageProps} />
