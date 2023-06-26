4cc3f35518523e215b1f5edf8e294accc3eb47c9
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-25 21:19:19 -0400

    style(TimDevIcon): change stroke-width to strokeWidth style(Hero): remove border from image chore(package.json): update dependencies to latest versions

--- components/Hero/component.tsx
+++ components/Hero/component.tsx
@@ -14,7 +14,7 @@ export const Hero: FC = () => {
       <motion.img
         src="/images/me-final.png"
         alt="me"
-        className="border border-gray-700 rounded-full w-28 h-28 md:w-32 md:h-32 mt-36"
+        className="rounded-full w-28 h-28 md:w-32 md:h-32 mt-36"
         variants={{
           hidden: {
             scale: 0,
